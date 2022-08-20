0a5afd230085835cebff3288043d2732df016465
Andrey Kolesin <kolesinandrei1910@gmail.com>
2022-08-20 14:31:48 +0300

    small fixes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import https from 'https'
 import cors from 'cors'
 
-// import db from './models/dbInstance'
+import db from './models/dbInstance'
 import router from './router'
 import { addResponseOkFuncToResObject } from './middlewares'
 import { errorHandler } from './errors'
@@ -21,11 +22,16 @@ app.use(errorHandler)
 
 const httpsServer = https.createServer(credentials, app)
 
-try{
-    httpsServer.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-    })
+
+async function start() {
+    try{
+        httpsServer.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    }
+    catch(e){
+        console.log(e)
+    }
+
 }
-catch(e){
-    console.log(e)
-}
+db.sequelize.sync({ alter: true }).then(start)
